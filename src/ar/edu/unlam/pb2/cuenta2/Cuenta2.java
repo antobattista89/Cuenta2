209af45835acd3d43d9ba9fd059214e0584e9b1b
ANTO <anbattista@alumno.unlam.edu.ar>
2021-05-25 17:46:59 -0300

    tercer commit cuenta2

--- src/ar/edu/unlam/pb2/cuenta2/Cuenta2.java
+++ src/ar/edu/unlam/pb2/cuenta2/Cuenta2.java
@@ -5,9 +5,19 @@ public abstract class Cuenta2 {
 	protected String titular;
 	protected Double saldo;
 	
+	Cuenta2(String titular){
+		this.titular=titular;
+		this.saldo=0.0;		
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	protected void depositarMonto(Double monto) {
+		this.saldo+=monto;
+	}
+	public abstract void retirarDinero(Double monto); 
 	
-	
-	
-	
-	
+	public abstract String getTipoCuenta(); 
 }
